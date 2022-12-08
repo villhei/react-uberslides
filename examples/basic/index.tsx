8fd9b4173f70523a0324682ef885278eb6c13507
Ville Heikkinen <ville.heikkinen@compile.fi>
2022-12-08 21:33:41 +0200

    chore: Migrate to ReactV18 api

--- examples/basic/index.tsx
+++ examples/basic/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = document.getElementById("app")!;
-
-ReactDOM.render(<App />, root);
+const container = document.getElementById("app")!;
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
