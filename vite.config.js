71863fd3a0311efe0b4a21485a6bcfabc404be3f
Ville Heikkinen <ville.heikkinen@gmail.com>
2023-01-23 21:32:29 +0200

    chore: Include type declarations in build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
@@ -23,4 +24,9 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
 });
